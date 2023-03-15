1639a51dc64623c9682a6111c622544cfbdeddca
Ashwin_Lenekar <xut243@corteva.com>
2023-03-15 10:50:28 +0100

    Commiting documentation

--- src/main/java/com/spring/demo/JavaSpringDemoApplication.java
+++ src/main/java/com/spring/demo/JavaSpringDemoApplication.java
@@ -4,17 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 @RestController
+
+@EnableWebMvc
 public class JavaSpringDemoApplication {
 
-	@GetMapping("/message")
-	public String message() {
-		return "Hello.. I am deployed.. !";
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(JavaSpringDemoApplication.class, args);
-	}
+    @GetMapping("/message")
+    public String message() {
+        return "Hello.. I am deployed.. !";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringDemoApplication.class, args);
+    }
 
 }
